b745bd81a18f1a8d3f921c3184344891fea0bc2d
Tommaso Catervi <56674392+tomctv@users.noreply.github.com>
2025-12-24 03:53:26 +0100

    feat: add basic top navigation

---
+++ src/components/global-top-navigation/index.tsx
@@ -0,0 +1,92 @@
+import { TopNavigation } from "@cloudscape-design/components";
+import { LogOutIcon } from "../icons/log-out";
+import { CalendarsIcon } from "../icons/calendars";
+import { UsersIcon } from "../icons/users";
+
+export const GlobalTopNavigation: React.FC = () => {
+  return (
+    <div id="h" style={{ position: "sticky", top: 0, zIndex: 1002 }}>
+      <TopNavigation
+        identity={{
+          href: "",
+          title: "Connect+",
+          logo: {
+            src: "src/assets/app-logo-small.svg",
+            alt: "Application",
+          },
+        }}
+        utilities={[
+          {
+            type: "button",
+            iconName: "search",
+            text: "Search",
+            ariaLabel: "Customer search",
+            disableUtilityCollapse: true,
+          },
+          {
+            type: "menu-dropdown",
+            iconSvg: <CalendarsIcon />,
+            ariaLabel: "Calendars",
+            title: "Calendars",
+            description:
+              "Manage your Outlook calendar and those of your coworkers.",
+            items: [
+              {
+                id: "calendar-my",
+                text: "My calendar",
+                secondaryText: "View your personal calendar",
+                iconName: "user-profile",
+              },
+              {
+                id: "calendar-team",
+                text: "Team calendar",
+                secondaryText: "View your team's calendar",
+                iconSvg: <UsersIcon />,
+              },
+            ],
+          },
+          {
+            type: "menu-dropdown",
+            text: "Username",
+            description: "email@example.com",
+            ariaLabel: "Manage profile",
+            iconName: "user-profile",
+            items: [
+              {
+                id: "role-group",
+                text: "Role",
+                ariaLabel: "Role selection",
+                items: [
+                  {
+                    id: "role-admin",
+                    text: "Admin",
+                    ariaLabel: " Admin role",
+                    iconName: "user-profile-active",
+                    labelTag: "\u2713",
+                    disabled: true,
+                    disabledReason: "You are already using this role",
+                    checked: true,
+                  },
+                  {
+                    id: "role-team-leader",
+                    text: "Team leader",
+                    ariaLabel: " Team leader role",
+                    iconName: "user-profile",
+                  },
+                  {
+                    id: "role-consultant",
+                    text: "Consultant",
+                    ariaLabel: " Consultant role",
+                    iconName: "user-profile",
+                  },
+                ],
+              },
+              { id: "preferences", text: "Preferences" },
+              { id: "signout", text: "Sign out", iconSvg: <LogOutIcon /> },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { GlobalTopNavigation } from "../components/global-top-navigation";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,7 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
-      <div>Hello "__root"!</div>
+      <GlobalTopNavigation />
       <Outlet />
     </React.Fragment>
   );
