361067cce49556255571f6adb55bc6fbfb936cf1
Tommaso Catervi <56674392+tomctv@users.noreply.github.com>
2025-12-24 01:47:55 +0100

    chore: set up cloudscape

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "@cloudscape-design/global-styles/index.css";
 import "./index.css";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import {
+  I18nProvider,
+  importMessages,
+} from "@cloudscape-design/components/i18n";
+
+// Get the messages for the current locale through "lang" on <html>:
+const locale = document.documentElement.lang;
+const messages = await importMessages(locale);
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -23,8 +32,10 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
-      <TanStackRouterDevtools router={router} initialIsOpen={false} />
+      <I18nProvider locale={locale} messages={messages}>
+        <RouterProvider router={router} />
+        <TanStackRouterDevtools router={router} initialIsOpen={false} />
+      </I18nProvider>
     </StrictMode>
   );
 }
