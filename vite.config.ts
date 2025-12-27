8e1aa8a801ba80cac6c735bdc9bcc26c011664bc
Tommaso Catervi <56674392+tomctv@users.noreply.github.com>
2025-12-27 21:09:07 +0100

    chore: set up src path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +12,9 @@ export default defineConfig({
     }),
     react(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
