6666ae05fb71503131736ed93b9c12ed8a5cb6e2
Tommaso Catervi <56674392+tomctv@users.noreply.github.com>
2026-02-21 15:50:53 +0100

    feat: add loading fallback component

--- src/routes/customers.search.tsx
+++ src/routes/customers.search.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { CustomerSearchParamsSchema } from "@/features/customer-search/schemas/customer-search-params.schema";
 import { CustomerSearchPage } from "@/features/customer-search/components/customer-search-page";
+import { LoadingFallback } from "@/components/loading-fallback";
 
 export const Route = createFileRoute("/customers/search")({
   validateSearch: CustomerSearchParamsSchema,
@@ -10,7 +11,7 @@ export const Route = createFileRoute("/customers/search")({
 
 function RouteComponent() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
+    <Suspense fallback={<LoadingFallback />}>
       <CustomerSearchPage />
     </Suspense>
   );
